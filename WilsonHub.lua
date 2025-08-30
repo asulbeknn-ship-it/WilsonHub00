57ba1e486517be5f750bc5595757abe2ff9a99df
asulbeknn-ship-it <asulbeknn@gmail.com>
2025-08-30 17:30:46 +0500

    WilsonHub.lua

--- WilsonHub.lua
+++ WilsonHub.lua
@@ -1231,13 +1231,50 @@ local WilsonHubGui=player.PlayerGui:FindFirstChild("WilsonHubGui")
 if WilsonHubGui then WilsonHubGui.Enabled=true end
 sendTranslatedNotification("notif_welcome_title", "notif_welcome_text", 7, "notif_welcome_button")
 
+-- [[ МУЗЫКАНЫ БАСҚАРУ ЖҮЙЕСІ ]]
+-- Бастапқы айнымалылар
 local soundId = "72089843969979"
-local playbackSpeed = 0.19 
+local playbackSpeed = 0.19
 local soundVolume = 6
+local soundOnIcon = "rbxassetid://96768815002144" -- Музыка қосулы иконкасы
+local soundOffIcon = "rbxassetid://125331517259500" -- Музыка өшірулі иконкасы
 
+-- Музыканы құру
 local audio = Instance.new("Sound", game.SoundService)
 audio.SoundId = "rbxassetid://" .. soundId
 audio.PlaybackSpeed = playbackSpeed
 audio.Volume = soundVolume
-audio.Looped = true -- Музыканы қайталау үшін осы қатарды қостық
+audio.Looped = true -- Музыкa қайталанып ойнайды
 audio:Play()
+
+-- Басқару батырмасын (кнопкасын) құру
+-- Егер WilsonHubGui табылмаса, қатенің алдын алу
+local playerGui = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+local WilsonHubGui = playerGui:FindFirstChild("WilsonHubGui")
+
+if WilsonHubGui then
+    local MainFrame = WilsonHubGui:FindFirstChild("MainFrame")
+    if MainFrame then
+        local MuteButton = Instance.new("ImageButton")
+        MuteButton.Name = "MuteButton"
+        MuteButton.Parent = MainFrame
+        MuteButton.BackgroundTransparency = 1
+        MuteButton.AnchorPoint = Vector2.new(1, 1) -- Оң жақ төменгі бұрышқа орнату
+        MuteButton.Position = UDim2.new(1, -15, 1, -15) -- Шегіністерді реттеу
+        MuteButton.Size = UDim2.new(0, 40, 0, 40) -- Өлшемін реттеу
+        MuteButton.Image = soundOnIcon -- Бастапқыда музыка қосулы тұрады
+
+        -- Батырманы басқанда не болатынын анықтайтын функция
+        MuteButton.MouseButton1Click:Connect(function()
+            if audio.IsPlaying then
+                -- Егер музыка ойнап тұрса, оны тоқтатып, иконканы өзгертеміз
+                audio:Pause()
+                MuteButton.Image = soundOffIcon
+            else
+                -- Егер музыка тоқтап тұрса, оны жалғастырып, иконканы қайтарамыз
+                audio:Resume()
+                MuteButton.Image = soundOnIcon
+            end
+        end)
+    end
+end
